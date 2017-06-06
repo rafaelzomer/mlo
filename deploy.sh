273f3fcd9ff789ad1c6a6271a5b6df0d59ac09d8
Rafael Zomer Antunes <rafaelzomer@gmail.com>
2017-06-05 21:31:43 -0300

    update deploy

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 STATUS="$(git status)"
 
-if [[ $STATUS == *"nothing to commit, working directory clean"* ]]
+if [[ $STATUS == *"nothing to commit"* ]]
 then
     sed -i "" '/dist/d' ./.gitignore
     git add .
