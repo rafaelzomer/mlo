253fb171ff165469c7bb3feab0ef3b0065be6ccf
Rafael Zomer Antunes <rafaelzomer@gmail.com>
2017-06-05 21:56:14 -0300

    deploy okay

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,3 @@
-STATUS="$(git status)"
-
-if [[ $STATUS == *"nothing to commit"* ]]
-then
-    sed -i "" '/dist/d' ./.gitignore
-    git add .
-    git commit -m "Edit .gitignore to publish"
-    git push origin `git subtree split --prefix dist master`:gh-pages --force
-    git reset HEAD~
-    git checkout .gitignore
-else
-    echo "Need clean working directory to publish"
-fi
+git subtree split --prefix dist -b gh-pages
+git push -f origin gh-pages:gh-pages
+git branch -D gh-pages
